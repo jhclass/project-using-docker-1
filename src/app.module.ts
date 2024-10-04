f935b93361f059dca3334700330d6a769cbf83da
jin-hyoung,Lee <jhcode0@naver.com>
2024-10-04 14:41:32 +0900

    set up GraphQL Apollo and completed testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { SampleResolver } from './sample/sample.resolver';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      //typePaths: ['./src/**/*.gql'], // 경로 설정을 제대로 하지 않으면 안됩니다.
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // 자동으로 gql 생성. typeDefs 폴더에 따로 추가해 주지 않아도 됩니다.
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SampleResolver],
 })
 export class AppModule {}

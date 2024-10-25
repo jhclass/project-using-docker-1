fad396393d3c3f2aa45001004c0064076a96fab0
jin-hyoung,Lee <jhcode0@naver.com>
2024-10-25 18:45:02 +0900

    added module ,resolver, service for createAdvice

--- src/advice-type/create-advice-type/create-advice-type.resolver.ts
+++ src/advice-type/create-advice-type/create-advice-type.resolver.ts
@@ -1,4 +1,31 @@
-import { Resolver } from "@nestjs/graphql";
+import { Args, Context, Int, Mutation, Resolver } from "@nestjs/graphql";
+import { CommonResponse } from "@src/result-dto/common-response.dto";
+import { CreateAdviceTypeService } from "./create-advice-type.service";
+import { GqlAuthGuard } from "@src/auth/gql-auth.guard";
+import { UseGuards } from "@nestjs/common";
 
 @Resolver()
-export class CreateAdviceTypeResolver {}
+export class CreateAdviceTypeResolver {
+  constructor(
+    private readonly createAdviceTypeService: CreateAdviceTypeService,
+  ) {}
+  @UseGuards(GqlAuthGuard)
+  @Mutation(() => CommonResponse)
+  async createAdviceType(
+    @Context() context: any,
+    @Args("type") type: string,
+    @Args("indexNum", { type: () => Int }) indexNum: number,
+    @Args("category") category?: string,
+    @Args("onOff", { nullable: true }) onOff?: string,
+    @Args("defaultValue", { nullable: true }) defaultValue?: string,
+  ): Promise<CommonResponse> {
+    return this.createAdviceTypeService.createAdviceTypeFunc(
+      context,
+      type,
+      indexNum,
+      category,
+      onOff,
+      defaultValue,
+    );
+  }
+}
--- src/advice-type/create-advice-type/create-advice-type.service.ts
+++ src/advice-type/create-advice-type/create-advice-type.service.ts
@@ -1,6 +1,54 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@src/prisma/prisma.service";
 
 @Injectable()
 export class CreateAdviceTypeService {
-  constructor() {}
+  constructor(private readonly client: PrismaService) {}
+  async createAdviceTypeFunc(
+    context: any,
+    type: string,
+    indexNum: number,
+    category?: string,
+    onOff?: string,
+    defaultValue?: string,
+  ) {
+    try {
+      const { user } = context.req;
+      if (onOff === "N") {
+        throw new Error(`onOff 상태는 무조건 "Y" 로 지정해야합니다.`);
+      }
+      const compareAT = await this.client.adviceType.findFirst({
+        where: {
+          type,
+          category,
+          onOff: onOff,
+          branchId: user?.branchId,
+        },
+      });
+      if (compareAT !== null) {
+        throw new Error(`중복되는 분야 입니다.`);
+      }
+      await this.client.adviceType.create({
+        data: {
+          type,
+          indexNum,
+          category,
+          onOff,
+          defaultValue,
+          branchId: user?.branchId,
+        },
+      });
+      return {
+        ok: true,
+        message: `정상적으로 등록 완료 되었습니다.`,
+      };
+    } catch (error) {
+      console.error(error.message);
+      return {
+        ok: false,
+        message: `에러발생! 에러메세지를 확인하세요.`,
+        error: `Error:${error.message}`,
+      };
+    }
+  }
 }

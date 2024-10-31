ca18a468a4b152cbf9921ac7b9b36e3ca943439a
Jinhyoung Lee <jhcode0@naver.com>
2024-10-31 16:12:23 +0900

    added ObjectType for table.dto

--- src/result-dto/table.dto.ts
+++ src/result-dto/table.dto.ts
@@ -1,4 +1,6 @@
 import { ObjectType, Field, Int } from "@nestjs/graphql";
+import { Career } from "@prisma/client";
+
 @ObjectType()
 export class Branch {
   @Field(() => Int)
@@ -74,6 +76,160 @@ export class ManageUser {
   PermissionsGranted?: PermissionsGranted[];
   @Field(() => [StudentState], { nullable: true })
   StudentState?: StudentState[];
+  @Field(() => [ConsultationMemo], { nullable: true })
+  ConsultationMemo?: ConsultationMemo[];
+  @Field(() => [Int], { nullable: true })
+  favoriteStudentState?: number[];
+  @Field(() => [Student], { nullable: true })
+  Student?: Student[];
+  @Field(() => [StudentMemo], { nullable: true })
+  StudentMemo?: StudentMemo[];
+  @Field(() => [StudentPayment], { nullable: true })
+  StudentPayment?: StudentPayment[];
+  @Field(() => [PaymentDetail], { nullable: true })
+  PaymentDetail?: PaymentDetail[];
+  @Field(() => [Lectures], { nullable: true })
+  Lectures?: Lectures[];
+  @Field(() => [Stamp], { nullable: true })
+  Stamp?: Stamp[];
+}
+
+//Student (수강생)
+@ObjectType()
+export class Student {
+  @Field(() => Int)
+  id: number;
+  @Field()
+  name: string;
+  @Field()
+  phoneNum1: string;
+  @Field({ nullable: true })
+  phoneNum2?: string;
+  @Field()
+  smsAgreement: string;
+  @Field()
+  writer: string;
+  @Field(() => String)
+  createdAt: Date;
+  @Field(() => String)
+  updatedAt: Date;
+  @Field(() => String, { nullable: true })
+  birthday?: Date;
+  @Field(() => ManageUser, { nullable: true })
+  manager?: ManageUser;
+  @Field(() => Int, { nullable: true })
+  managerUserId?: number;
+  @Field(() => [StudentMemo], { nullable: true })
+  studentMemo?: StudentMemo[];
+  @Field(() => [StudentPayment], { nullable: true })
+  studentPayment?: StudentPayment[];
+  @Field(() => String, { nullable: true })
+  lastModifiedTime?: Date;
+}
+
+//StudentPayment (수강신청)
+@ObjectType()
+export class StudentPayment {
+  @Field(() => Int)
+  id: number;
+  @Field(() => Int, { nullable: true })
+  seScore?: number;
+  @Field(() => Int, { nullable: true })
+  tuitionFee?: number;
+  @Field({ nullable: true })
+  discountAmount?: string;
+  @Field(() => Int, { nullable: true })
+  cashAmount?: number;
+  @Field(() => Int, { nullable: true })
+  cardAmount?: number;
+  @Field(() => Int, { nullable: true })
+  actualAmount?: number;
+  @Field(() => Int, { nullable: true })
+  unCollectedAmount?: number;
+  @Field(() => [String], { nullable: true })
+  receiptClassification?: string[];
+  @Field({ nullable: true })
+  paymentDate?: string;
+  @Field(() => Student, { nullable: true })
+  student?: Student;
+  @Field(() => Int)
+  studentId: number;
+  @Field(() => [PaymentDetail], { nullable: true })
+  paymentDetail?: PaymentDetail[];
+  @Field(() => ManageUser, { nullable: true })
+  processingManager?: ManageUser;
+  @Field(() => Int, { nullable: true })
+  processingManagerId?: number;
+  @Field({ nullable: true })
+  situationReport?: boolean;
+  @Field(() => Int, { nullable: true })
+  amountReceived?: number;
+  @Field(() => Subject, { nullable: true })
+  subject?: Subject;
+  @Field(() => Int, { nullable: true })
+  subjectId?: number;
+  @Field({ nullable: true })
+  campus?: string;
+  @Field({ nullable: true })
+  subDiv?: string;
+  @Field({ nullable: true })
+  courseComplete?: string;
+  @Field({ nullable: true })
+  employment?: string;
+  @Field({ nullable: true })
+  dueDate?: string;
+  @Field({ nullable: true })
+  classCode?: string;
+  @Field({ nullable: true })
+  lectureAssignment?: string;
+  @Field({ nullable: true })
+  createdAt?: string;
+  @Field({ nullable: true })
+  updatedAt?: string;
+  @Field({ nullable: true })
+  isWeekend?: string;
+  @Field(() => [Attendance], { nullable: true })
+  attendance?: Attendance[];
+  @Field(() => Branch, { nullable: true })
+  Branch?: Branch;
+  @Field(() => Int, { nullable: true })
+  branchId?: number;
+  @Field({ nullable: true })
+  mZipCode?: string;
+  @Field({ nullable: true })
+  mAddresses?: string;
+  @Field({ nullable: true })
+  mAddressDetail?: string;
+  @Field({ nullable: true })
+  dateOfDroppingOut?: string;
+  @Field({ nullable: true })
+  reasonFordroppingOut?: string;
+  @Field(() => [EmploymentStatus], { nullable: true })
+  EmploymentStatus?: EmploymentStatus[];
+  @Field(() => [EduInfomation], { nullable: true })
+  EduInfomation?: EduInfomation[];
+  @Field(() => [Career], { nullable: true })
+  Career?: Career[];
+  @Field(() => [Certificate], { nullable: true })
+  Certificate?: Certificate[];
+  @Field(() => [StudentConsultation], { nullable: true })
+  StudentConsultation?: StudentConsultation[];
+  @Field(() => [HopeForEmployment], { nullable: true })
+  HopeForEmployment?: HopeForEmployment[];
+  @Field(() => [EmploymentRecommendation], { nullable: true })
+  EmploymentRecommendation?: [EmploymentRecommendation];
+  @Field(() => [PreInspection], { nullable: true })
+  PreInspection?: PreInspection[];
+  @Field(() => [StudentPortfolio], { nullable: true })
+  StudentPortfolio?: StudentPortfolio[];
+  @Field({ nullable: true })
+  supportType?: string;
+  @Field({ nullable: true })
+  lastModifiedTime?: string;
+  @Field({ nullable: true })
+  lastModifiedByName?: string;
+  @Field({ nullable: true })
+  lastModifiedByUserId?: string;
 }
 
 //StudentState (상담관리)
